b9134464a862ae326764eb79b39d88dabd49cb66
Bayron Urbina <183458548+bayronUE@users.noreply.github.com>
2025-02-23 19:27:38 -0500

    agregue las carpetas correspondientes

--- README.md
+++
@@ -1,2 +0,0 @@
-# taller1
-En este repositorio vamos a hacer la automatizacion de una tarea con un script
--- automatizar.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-#crear texto
-if[!-f"tareas.txt"];then
-	touch tareas.txt
-#segunda tarea
-fi
